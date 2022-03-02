3e64f894aa3f530ea94d164057a61557621a5c92
JavaharlalNehru <grechanichenkos@yandex.ru>
2022-03-02 12:01:16 +0300

    first commit

---
+++ source/target/debug/Polytope4D.d
@@ -0,0 +1,1 @@
+/home/javaharlalnehru/Документы/проект/Polytope4D/source/target/debug/Polytope4D: /home/javaharlalnehru/Документы/проект/Polytope4D/source/src/main.rs
---
+++ source/target/debug/deps/Polytope4D-185e361726e33263.d
@@ -0,0 +1,5 @@
+/home/javaharlalnehru/Документы/проект/Polytope4D/source/target/debug/deps/Polytope4D-185e361726e33263: src/main.rs
+
+/home/javaharlalnehru/Документы/проект/Polytope4D/source/target/debug/deps/Polytope4D-185e361726e33263.d: src/main.rs
+
+src/main.rs:
