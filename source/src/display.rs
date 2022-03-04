f77be73df013258c46ff696aadf93a1764820dcf
JavaharlalNehru <grechanichenkos@yandex.ru>
2022-03-04 12:28:17 +0300

    added struct Face

--- source/src/display.rs
+++ source/src/display.rs
@@ -5,18 +5,24 @@ use std::fmt::Formatter;
 
 impl Display for Vec4f {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "V({}, {}, {}, {})", self.x, self.y, self.z, self.w)
+        write!(f, "Vertice({}, {}, {}, {})", self.x, self.y, self.z, self.w)
     }
 }
 
 impl Display for Edge {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "E(\n  {},\n  {}\n)", self.a, self.b)
+        write!(f, "Edge(\n  {},\n  {}\n)", self.a, self.b)
     }
 }
 
 impl Display for Face {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        write!(f, "F(\n  {},\n  {},\n  {},\n)", self.edges[0], self.edges[1], self.edges[2])
+        write!(f, "Face-----\n  {},\n  {},\n  {},\n-----", self.edges[0], self.edges[1], self.edges[2])
+    }
+}
+
+impl Display for Cell {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "Cell=====\n  {},\n  {},\n  {},\n  {}\n=====", self.faces[0], self.faces[1], self.faces[2], self.faces[3])
     }
 }
--- source/src/main.rs
+++ source/src/main.rs
@@ -3,11 +3,17 @@ mod edge;
 mod face;
 mod display;
 mod comparison;
+mod cell;
+use cell::*;
 use vec4f::*;
 use edge::*;
 use face::*;
+use macroquad::prelude::*;
 
-fn main() {
-    let v = Vec4f::newf(0.0);
-    println!("{}", v);
+#[macroquad::main("Polytope 4D")]
+async fn main() {
+    let v1 = Vec4f::newf(0.0);
+    let v2 = Vec4f::newf(0.0);
+    let v3 = Vec4f::newf(0.0);
+    println!("{}", Face::from_verts(vec![v1, v2, v3]));
 }
