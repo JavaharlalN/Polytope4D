fe78fa4a6024971bd460db46e674c2632087f215
JavaharlalNehru <grechanichenkos@yandex.ru>
2022-03-05 18:35:27 +0300

    cursor hidden!

---
+++ objects/target/rls/debug/deps/objects-8e65f92ddad89be4.d
@@ -0,0 +1,12 @@
+/home/javaharlalnehru/Документы/проект/Polytope4D/objects/target/rls/debug/deps/objects-8e65f92ddad89be4.rmeta: src/lib.rs src/display.rs src/vector.rs src/edge.rs src/face.rs src/cell.rs src/comparison.rs src/rotation.rs
+
+/home/javaharlalnehru/Документы/проект/Polytope4D/objects/target/rls/debug/deps/objects-8e65f92ddad89be4.d: src/lib.rs src/display.rs src/vector.rs src/edge.rs src/face.rs src/cell.rs src/comparison.rs src/rotation.rs
+
+src/lib.rs:
+src/display.rs:
+src/vector.rs:
+src/edge.rs:
+src/face.rs:
+src/cell.rs:
+src/comparison.rs:
+src/rotation.rs:
--- source/src/main.rs
+++ source/src/main.rs
@@ -1,19 +1,15 @@
-mod vec4f;
-mod edge;
-mod face;
-mod display;
-mod comparison;
-mod cell;
-mod window;
-mod field;
-use cell::*;
-use vec4f::*;
-use edge::*;
-use face::*;
+use objects::*;
 use macroquad::prelude::*;
+use ::miniquad::Context;
+
+extern "system" {
+    
+}
 
 #[macroquad::main("Polytope 4D")]
 async fn main() {
+    let ctx = Context::new();
+    ctx.show_mouse(false);
     loop {
         clear_background(Color::new(1.0, 1.0, 1.0, 1.0));
         next_frame().await
