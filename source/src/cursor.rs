d3a943923d9acfdcba8422361e04344d04e44a24
JavaharlalNehru <grechanichenkos@yandex.ru>
2022-03-06 00:40:36 +0300

    added cursor

---
+++ source/src/cursor.rs
@@ -0,0 +1,53 @@
+const STEPS: u8 = 10;
+
+#[derive(Copy, Clone)]
+pub struct Config {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
+    pub r: f32,
+}
+
+#[derive(Copy, Clone)]
+pub struct Cursor {
+    pub conf: Config,
+    pub rect: bool,
+    pub last: Config,
+    pub need: Config,
+    pub step: u8,
+}
+
+impl Config {
+    fn default() -> Config {
+        Config{
+            x: 0.0,
+            y: 0.0,
+            w: 3.0,
+            h: 3.0,
+            r: 3.0,
+        }
+    }
+
+    fn with_pos(pos: (f32, f32)) -> Config {
+        Config{
+            x: pos.0,
+            y: pos.1,
+            w: 3.0,
+            h: 3.0,
+            r: 3.0,
+        }
+    }
+}
+
+impl Cursor {
+    pub fn new(pos: (f32, f32)) -> Cursor {
+        Cursor{
+            conf: Config::with_pos(pos),
+            rect: false,
+            last: Config::default(),
+            need: Config::default(),
+            step: 0,
+        }
+    }
+}
--- source/src/main.rs
+++ source/src/main.rs
@@ -1,6 +1,3 @@
-use objects::*;
-use macroquad::prelude::*;
-// use ::miniquad::Context;
 #[cfg(target_os = "macos")]
 extern crate sapp_darwin as sapp;
 #[cfg(not(any(
@@ -16,14 +13,27 @@ extern crate sapp_linux as sapp;
 extern crate sapp_wasm as sapp;
 #[cfg(windows)]
 extern crate sapp_windows as sapp;
+mod cursor;
+use macroquad::prelude::*;
+use macroquad::input::mouse_position;
+use cursor::*;
+use objects::*;
+use window::*;
 
 #[macroquad::main("Polytope 4D")]
 async fn main() {
     unsafe {
         sapp::sapp_show_mouse(false);
     }
+    let main_window = MainWindow::new(screen_width(), screen_height());
+    let mut cursor = Cursor::new(mouse_position());
     loop {
-        clear_background(Color::new(1.0, 1.0, 1.0, 1.0));
+        let (x_pos, y_pos) = mouse_position();
+        clear_background(Color::new(0.8, 0.8, 0.8, 1.0));
+        // draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        cursor.conf.x = x_pos;
+        cursor.conf.y = y_pos;
+        draw_poly(cursor.conf.x, cursor.conf.y, 10, cursor.conf.r, 0.0, Color::new(0.3, 0.3, 0.3, 1.0));
         next_frame().await
     }
 }
